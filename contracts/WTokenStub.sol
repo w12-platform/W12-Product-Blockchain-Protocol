52c3990b65c64e1fbc0ba804a87f264b08e12027
Serge Sarkosyan <39399632+waramas@users.noreply.github.com>
2018-07-18 01:22:26 +0300

    Implementation of missed methods

---
+++ contracts/WTokenStub.sol
@@ -0,0 +1,15 @@
+pragma solidity ^0.4.23;
+
+import "./WToken.sol";
+
+contract WTokenStub is WToken {
+
+    /**
+    * @dev Allows for any account besides the owner.
+    */
+    modifier onlyOwner() {
+        _;
+    }
+
+    constructor(string _name, string _symbol, uint8 _decimals) WToken(_name, _symbol, _decimals) public { }
+}
