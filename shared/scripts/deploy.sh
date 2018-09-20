1b0db2bab9500d8aad79a64e8a22ef5584ce450a
Kirill Khoroshilov <mr.hokid@gmail.com>
2018-09-20 22:45:11 +0700

    ci: update
    [ci skip]

--- shared/scripts/deploy.sh
+++ shared/scripts/deploy.sh
@@ -2,5 +2,5 @@
 cp example.config.js config.js
 npm run t:migrate:r:test
 git add .
-git commit -m "chore: migration of version $(npm run --silent version)"
+git commit -m "chore: migration of version $(npm run --silent version) [ci skip]"
 git push origin HEAD:master
