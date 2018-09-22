e498f72a34d51d8f5057bad76203c2152b278d05
Hokid <mr.hokid@gmail.com>
2018-09-22 20:02:22 +0700

    ci: force commit build result

--- shared/scripts/deploy.sh
+++ shared/scripts/deploy.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/env bash
 cp example.config.js config.js
 npm run t:migrate:r:test
-git add .
+# because `build/` in gitignore
+git add -f build/
 git commit -m "chore: migration of version $(npm run --silent version) [ci skip]"
+# push from detached head
 git push origin HEAD:master
