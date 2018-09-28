13f6999cbfa237efc8e8f55b56e6afcc71dc0571
Hokid <mr.hokid@gmail.com>
2018-09-28 08:07:34 +0100

    feat: add rates contract (#48)
    * feat: add rates contract
    * feat: improve performance
    * feat: small optimization
    * feat: add ability to fetch array of symbols
    * feat: replace command
    * feat: add tests

---
+++ contracts/rates/Symbols.sol
@@ -0,0 +1,41 @@
+pragma solidity ^0.4.24;
+
+contract Symbols {
+    bytes32[] public symbolsList;
+    mapping (bytes32 => uint) symbolIndex;
+    mapping (bytes32 => bool) symbols;
+
+    function addSymbol(bytes32 symbol) public {
+        require(!symbols[symbol]);
+
+        symbols[symbol] = true;
+        symbolIndex[symbol] = symbolsList.length;
+        symbolsList.push(symbol);
+    }
+
+    function removeSymbol(bytes32 symbol) public {
+        require(symbols[symbol]);
+
+        symbols[symbol] = false;
+        _removeSymbolByIndex(symbolIndex[symbol]);
+        symbolIndex[symbol] = 0;
+    }
+
+    function hasSymbol(bytes32 symbol) public view returns(bool) {
+        return symbols[symbol];
+    }
+
+    function getSymbolsList() public view returns(bytes32[]) {
+        return symbolsList;
+    }
+
+    function _removeSymbolByIndex(uint index) internal {
+        require(index < symbolsList.length);
+
+        if (index != symbolsList.length - 1) {
+            symbolsList[index] = symbolsList[symbolsList.length - 1];
+        }
+
+        symbolsList.length--;
+    }
+}
---
+++ contracts/rates/roles/Pricer.sol
@@ -0,0 +1,47 @@
+pragma solidity ^0.4.24;
+
+import "openzeppelin-solidity/contracts/access/rbac/Roles.sol";
+
+contract PricerRole {
+    using Roles for Roles.Role;
+
+    event PricerAdded(address indexed account);
+    event PricerRemoved(address indexed account);
+
+    Roles.Role private pricers;
+
+    constructor() public {
+        _addPricer(msg.sender);
+    }
+
+    modifier onlyPricer() {
+        require(isPricer(msg.sender));
+        _;
+    }
+
+    function isPricer(address account) public view returns (bool) {
+        return pricers.has(account);
+    }
+
+    function addPricer(address account) public onlyPricer {
+        _addPricer(account);
+    }
+
+    function removePricer(address account) public {
+        _removePricer(account);
+    }
+
+    function _addPricer(address account) internal {
+        require(account != address(0));
+
+        pricers.add(account);
+        emit PricerAdded(account);
+    }
+
+    function _removePricer(address account) internal {
+        require(account != address(0));
+
+        pricers.remove(account);
+        emit PricerRemoved(account);
+    }
+}
