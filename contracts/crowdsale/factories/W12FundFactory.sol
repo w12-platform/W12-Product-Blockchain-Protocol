2f3d1ec53a2382b3f0c30b655dff1ad761084583
Kirill Khoroshilov <mr.hokid@gmail.com>
2018-10-10 01:51:37 +0700

    feat(Fund): add tranche realise with different assets

--- contracts/crowdsale/factories/W12FundFactory.sol
+++ contracts/crowdsale/factories/W12FundFactory.sol
@@ -5,12 +5,16 @@ import "../W12Fund.sol";
 import "../../versioning/Versionable.sol";
 
 contract W12FundFactory is Versionable, IW12FundFactory {
+    IRates rates;
+
     event FundCreated(address indexed fund);
 
-    constructor(uint version) Versionable(version) public {}
+    constructor(uint version, IRates _rates) Versionable(version) public {
+        rates = _rates;
+    }
 
     function createFund(address swap, address serviceWallet, uint trancheFeePercent) external returns (IW12Fund result) {
-        result = new W12Fund(version, trancheFeePercent);
+        result = new W12Fund(version, trancheFeePercent, rates);
 
         result.setSwap(swap);
         result.setServiceWallet(serviceWallet);
