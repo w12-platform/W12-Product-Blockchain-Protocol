dee52de4ba60207d6763999951d50966d6b0040d
Kirill Khoroshilov <mr.hokid@gmail.com>
2018-10-16 19:55:16 +0700

    ci: update deploy script to test net
     * improve script
     * add git tag generation
     * add release notes generation

---
+++ shared/scripts/deploy-test-net.sh
@@ -0,0 +1,32 @@
+#!/usr/bin/env bash
+set -eo pipefail
+
+cp example.config.js config.js
+
+VERSION=$(npm run --silent version);
+VERSION_TAG="v$VERSION-alpha";
+
+npm run --silent t:migrate:r:test 2>&1 | tee ".MIGRATE_LOG_v$VERSION"
+
+MIGRATE_LOG=$(cat ".MIGRATE_LOG_v$VERSION");
+ADDRESSES=$(cat ".MIGRATE_v$VERSION");
+
+npm run abi
+
+# because `build/` in gitignore
+git add -f build/
+git add abi/
+
+git commit -m \
+"chore: migration of version $VERSION_TAG [ci skip]"\
+$'\n'\
+"LOG: $MIGRATE_LOG"\
+$'\n'\
+"MIGRATION:"\
+$'\n'\
+"$ADDRESSES"
+
+git tag "$VERSION_TAG"
+git push --tags origin HEAD:master
+
+npm run gen-release
