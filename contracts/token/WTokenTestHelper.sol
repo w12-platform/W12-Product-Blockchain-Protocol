6f56402401f51679a98e8c4e026ade22b0237ec9
Andrey Zaytsev <leyte@live.ru>
2018-10-25 21:47:29 +0700

    fix: typo
    Co-Authored-By: Hokid <mr.hokid@gmail.com>

--- contracts/token/WTokenTestHelper.sol
+++ contracts/token/WTokenTestHelper.sol
@@ -10,7 +10,7 @@ contract WTokenTestHelper {
     event NewToken(address indexed tokenAddress);
 
     function createToken(string _name, string _symbol, uint8 _decimals, uint amountToIssue) public returns(WToken token) {
-        require(_isStingValid(_name, 5, 50));
+        require(_isStringValid(_name, 5, 50));
         require(_isStingValid(_symbol, 3, 5));
         require(_decimals > uint8(1) && _decimals < uint8(19));
         require(amountToIssue > 99 && amountToIssue <= (uint(-1) / 10 ** uint(_decimals)));
