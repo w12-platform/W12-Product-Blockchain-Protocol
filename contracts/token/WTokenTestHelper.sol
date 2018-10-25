44ee6257707ab14778a72740f4146171b7cafd0c
Hokid <mr.hokid@gmail.com>
2018-10-25 23:59:38 +0700

    feat(TokenFactory): [W12-98] add token parameters validation (#57)
    * feat(TokenFactory): [W12-98] add token parameters validation

--- contracts/token/WTokenTestHelper.sol
+++ contracts/token/WTokenTestHelper.sol
@@ -10,15 +10,18 @@ contract WTokenTestHelper {
     event NewToken(address indexed tokenAddress);
 
     function createToken(string _name, string _symbol, uint8 _decimals, uint amountToIssue) public returns(WToken token) {
+        require(_isStringValid(_name, 5, 50));
+        require(_isStringValid(_symbol, 3, 5));
+        require(_decimals > uint8(1) && _decimals < uint8(19));
+        require(amountToIssue > 99 && amountToIssue <= (uint(-1) / 10 ** uint(_decimals)));
+
         token = new WToken(_name, _symbol, _decimals);
         token.transferOwnership(msg.sender);
 
         tokenIndexes[address(token)] = tokens.length;
         tokens.push(address(token));
 
-        if (amountToIssue > 0) {
-            token.mint(msg.sender, amountToIssue * 10 ** uint(_decimals), 0);
-        }
+        token.mint(msg.sender, amountToIssue * 10 ** uint(_decimals), 0);
 
         emit NewToken(token);
     }
@@ -49,5 +52,29 @@ contract WTokenTestHelper {
         require(hasToken(tokenAddress));
 
         return WToken(tokenAddress).totalSupply();
+    }
+
+    function _isStringValid(string _string, uint minLn, uint maxLn) internal pure returns(bool) {
+        require(maxLn >= minLn);
+
+        bytes memory _stringBytes = bytes(_string);
+
+        // allow empty
+        if (_stringBytes.length == 0 && minLn == 0) return true;
+        // not match length
+        if (_stringBytes.length < minLn || _stringBytes.length > maxLn) return;
+        // spaces at beginning and ending
+        if (_stringBytes[0] == 0x20 || _stringBytes[_stringBytes.length - 1] == 0x20) return;
+
+        for(uint i = 0; i < _stringBytes.length; i++) {
+            if (
+                !(_stringBytes[i] == 0x20 && _stringBytes[i + 1] != 0x20)  // space, not followed by space
+                && !(_stringBytes[i] >= 0x30 && _stringBytes[i] <= 0x39) // 0-9
+                && !(_stringBytes[i] >= 0x41 && _stringBytes[i] <= 0x5a) // A-Z
+                && !(_stringBytes[i] >= 0x61 && _stringBytes[i] <= 0x7a) // a-z
+            ) return false;
+        }
+
+        return true;
     }
 }
