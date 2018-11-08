41c4d731cd62cdb3afcb507e4e989f7c8fb7e01f
Hokid <mr.hokid@gmail.com>
2018-11-08 16:02:18 +0700

    ci: [W12-133] send notification into telegram channel after deploy was done (#65)

--- shared/scripts/deploy-test-net.sh
+++ shared/scripts/deploy-test-net.sh
@@ -4,7 +4,7 @@ set -eo pipefail
 cp example.config.js config.js
 
 VERSION=$(npm run --silent version);
-VERSION_TAG="v$VERSION-alpha";
+VERSION_TAG="v$VERSION-alpha.0";
 
 npm run --silent t:migrate:r:test 2>&1 | tee ".MIGRATE_LOG_v$VERSION"
 
@@ -33,3 +33,11 @@ git push origin HEAD:alpha-release
 git push origin "$VERSION_TAG"
 
 npm run gen-release
+
+chmod +x ./shared/scripts/notify.sh
+./shared/scripts/notify.sh \
+"w12-platform/W12-Product-Blockchain-Protocol $VERSION_TAG"\
+$'\n\n'\
+"https://github.com/w12-platform/W12-Product-Blockchain-Protocol/releases/tag/$VERSION_TAG"\
+$'\n\n'\
+"$ADDRESSES"
